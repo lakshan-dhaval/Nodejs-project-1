e279153bcdcaf94a1c9348272901d69909f492ee
lakshan-dhaval <lakshandhaval07@gmail.com>
2025-03-25 14:39:51 +0530

    Update README.md

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ const app=express();
 
 connectDB()
 
+// app call kiya hai connectDB()
+
 .then (() => {
     app.listen(process.env.PORT || 8000, () => {
         console.log(`Server is running at port : ${process.env.PORT}`);
--- src/utils/ascyncHandler.js
+++ src/utils/ascyncHandler.js
@@ -1,1 +1,8 @@
-const asyncHandler = () => {}
+const asyncHandler = (fn) => async (req, res, next) =>{
+    try {
+        await fn(req, res, next)
+    } catch (error) {
+        console.log(error);
+        // message:error.message
+    }
+}
